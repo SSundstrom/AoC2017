a2d7556c416932d915cdb719ab51334a0e498f35
Simon Sundström <ssundstrom@live.se>
2017-12-02 16:36:09 +0100

    fixed input

--- parsers.hs
+++ parsers.hs
@@ -1,8 +1,14 @@
 module Parsers where
+import Data.Char
 
-intsByRowWord :: ([[Int]] -> Int) -> IO ()
-intsByRowWord f = do
-    c <- readFile "input"
+intsByRowWord :: ([[Int]] -> Int) -> FilePath -> IO ()
+intsByRowWord f p = do
+    c <- readFile p
     let parsedString = map words $ lines c
     let parsedInt = map (map (\x -> read x ::Int)) parsedString
     print $ f parsedInt
+
+intsFromString :: ([Int] -> Int) -> FilePath -> IO ()
+intsFromString f p = do
+    c <- readFile p
+    print $ f $ map digitToInt c
