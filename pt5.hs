08a282185a8c371a9dc54a76858246bfa09e8c37
Simon Sundström <ssundstrom@live.se>
2017-12-05 17:32:13 +0100

    all the versions.meme

--- pt5.hs
+++ pt5.hs
@@ -1,50 +1,91 @@
 module Main where
 import Parsers
-import Data.IntMap
+import Data.Map.Strict as A
+import Data.IntMap as B
     
 main = both
 
-
 pt1 :: IO ()
 pt1 = do
-    input <- intsByWord 5
-    let imap = fromList (zip [x | x <- [0..length input]] input)
+    imap <- getintMap
     print "Part 1"
-    timed (step (size imap) 0 imap)
+    timed (step (A.size imap) 0 imap)
     
 
 pt2 :: IO ()
 pt2 = do
-    input <- intsByWord 5
-    let imap = fromList (zip [x | x <- [0..length input]] input)
-    timed (step' (size imap) 0 imap)
+    imap <- getintMap
+    timed (step1i (A.size imap) 0 imap)
     
 
 
 both :: IO ()
 both = do
+    imap <- getintMap
+    putStrLn "Part 1"
+    timed $ step (A.size imap) 0 imap
+    putStrLn "Part 2"
+    putStrLn "--------------------"
+    putStrLn "map insert"
+    timed $ step1i (A.size imap) 0 imap
+    putStrLn "--------------------"    
+    putStrLn "map update"
+    timed $ step1u (A.size imap) 0 imap
+    putStrLn "--------------------"    
+    iimap <- getIntMap
+    putStrLn "intMap insert"
+    timed $ step2i (B.size iimap) 0 iimap
+    putStrLn "--------------------"    
+    putStrLn "intMap update"
+    timed $ step2u (B.size iimap) 0 iimap
+    
+getintMap :: IO (Map Int Int)
+getintMap = do
     input <- intsByWord 5
-    let imap = fromList (zip [x | x <- [0..length input]] input)
-    print "Part 1"
-    timed $ step (size imap) 0 imap
-    print "Part 2"
-    timed $ step' (size imap) 0 imap
+    return $ A.fromList (zip [0 .. length input] input)
+
+getIntMap :: IO (IntMap Int)
+getIntMap = do
+    input <- intsByWord 5
+    return $ B.fromList (zip [0 .. length input] input)
     
 
-
-step :: Int -> Int -> IntMap Int -> Int
+step :: Int -> Int -> Map Int Int -> Int
 step s i l 
     | s <= i = 0
-    | otherwise = 1 + step s (i + v) (update (\x -> Just (x+1)) i l)
-    where v = l!i
+    | otherwise = 1 + step s (i + v) (A.update (\x -> Just (x+1)) i l)
+    where v = l A.! i
 
-step' :: Int -> Int -> IntMap Int -> Int
-step' s i l 
+step1u :: Int -> Int -> Map Int Int -> Int
+step1u s i l 
     | s <= i = 0
-    | v > 2 = 1 + step' s (i + v) (update (\x -> Just (x-1)) i l)
-    | otherwise = 1 + step' s (i + v) (update (\x -> Just (x+1)) i l)
-    where v = l!i
+    | v > 2 = 1 + step1u s (i + v) (A.update (\x -> Just (x-1)) i l)
+    | otherwise = 1 + step1u s (i + v) (A.update (\x -> Just (x+1)) i l)
+    where v = l A.! i
     
+step1i :: Int -> Int -> Map Int Int -> Int
+step1i s i l 
+    | s <= i = 0
+    | v > 2 = 1 + step1i s (i + v) (A.insert i (v-1) l)
+    | otherwise = 1 + step1i s (i + v) (A.insert i (v+1) l)
+    where v = l A.! i
+
+step2u :: Int -> Int -> IntMap Int -> Int
+step2u s i l 
+    | s <= i = 0
+    | v > 2 = 1 + step2u s (i + v) (B.update (\x -> Just (x-1)) i l)
+    | otherwise = 1 + step2u s (i + v) (B.update (\x -> Just (x+1)) i l)
+    where v = l B.! i
+
+
+step2i :: Int -> Int -> IntMap Int -> Int
+step2i s i l 
+    | s <= i = 0
+    | v > 2 = 1 + step2i s (i + v) (B.insert i (v-1) l)
+    | otherwise = 1 + step2i s (i + v) (B.insert i (v+1) l)
+    where v = l B.! i
+    
+
 {-
 incElem :: Int -> [Int] -> [Int]
 incElem i l = concat [x, [(y!!0)+1], drop 1 y]
